c4a47b41d70a3d0444daa4cbd6277259a8db1c3c
stirlingx <stirlingx@git.coding.net>
2017-01-18 12:04:46 +0800

    Add client lib

---
+++ client/client.go
@@ -0,0 +1,65 @@
+package client
+
+import (
+	"github.com/codinl/go-logger"
+	"google.golang.org/grpc"
+	"github.com/liyue201/gmcache/proto"
+	"context"
+)
+
+type Client struct {
+	addr string
+	conn *grpc.ClientConn
+}
+
+func NewClient(addr string) *Client {
+	return &Client{addr: addr}
+}
+
+func (this *Client) Connect() error {
+	c, err := grpc.Dial(this.addr, grpc.WithInsecure())
+	if err != nil {
+		logger.Errorf("grpc dial: %s", err.Error())
+		return err
+	}
+	this.conn = c
+	return nil
+}
+
+func (this *Client) Disconnect() error {
+	if this.conn != nil {
+		err := this.conn.Close()
+		this.conn = nil
+		return  err
+	}
+	return nil
+}
+
+func (this *Client) Set(key string, value []byte, ttl uint64) error {
+	arg := &proto.SetOptArg{
+		Key: key,
+		Val: value,
+		Ttl: ttl,
+	}
+	c := proto.NewRpcServiceClient(this.conn)
+	_, err := c.Set(context.Background(), arg)
+	return err
+}
+
+func (this *Client) Get(key string) (value []byte, err error) {
+	arg := &proto.GetOptArg{
+		Key: key,
+	}
+	c := proto.NewRpcServiceClient(this.conn)
+	ret, err := c.Get(context.Background(), arg)
+	return ret.Val, err
+}
+
+func (this *Client) Delete(key string) error {
+	arg := &proto.DelOptArg{
+		Key: key,
+	}
+	c := proto.NewRpcServiceClient(this.conn)
+	_, err := c.Delete(context.Background(), arg)
+	return err
+}
---
+++ client/example/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/liyue201/gmcache/client"
+	"log"
+)
+
+func main()  {
+	c :=  client.NewClient("127.0.0.1:8002")
+	if err := c.Connect(); err != nil {
+		log.Println(err)
+		return
+	}
+	if err := c.Set("aaa", []byte("bbbbbbb"), 10); err != nil {
+		log.Println(err)
+		return
+	}
+	val, err := c.Get("aaa")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(string(val))
+
+	err = c.Delete("aaa")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	c.Disconnect()
+
+}
